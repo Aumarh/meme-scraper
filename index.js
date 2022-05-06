f828a1b8d4cc8cf06c753360e3ce6f604319a5c4
Aumarh <aumarh@riseup.net>
2022-05-06 16:20:30 +0200

    Add image working version

--- index.js
+++ index.js
@@ -1,93 +1,61 @@
 // importing required library
-import axios from 'axios';
-// import cheerio from 'cheerio';
-// import { data } from 'cheerio/lib/api/attributes';
-import fetch from 'fetch';
-import fs from 'fs';
+import * as fs from 'fs';
 import https from 'https';
+import fetch from 'node-fetch';
 
-// creating the folder for the meme img
-// fs.mkdirSync('./path/to');
+// creating the meme folder
+const path = './memes';
 
-// defining variables
-// const pageUrl = ;
-// const memeUrl =
-//   'https://memecomplete.com/edit/https:/api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg';
-// const meme = memeUrl + '.images';
-// const wait = (time) => new promises((resolve) => setTimeout(resolve, time));
-
-// const downloadMemeInPage = async function(page)
-
-// importing HTML
-axios
-  .get('https://memegen-link-examples-upleveled.netlify.app/')
-
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-const url =
-  'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300';
-
-https.get(url, (res) => {
-  const path = '01.jpg';
-  const writeStream = fs.createWriteStream(path);
-
-  res.pipe(writeStream);
-
-  writeStream.on('finish', () => {
-    writeStream.close();
-    console.log('Download Complete');
-  });
+// To check if the given directory
+// already exists or not
+fs.access(path, (error) => {
+  if (error) {
+    // If current directory does not exist
+    // then create it
+    fs.mkdir(path, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('New Directory created successfully!!');
+      }
+    });
+  } else {
+    console.log('Directory already exists !!');
+  }
 });
 
-// let getData = (html) => {
-//   data = [];
-//   const $ = cheerio.load(html);
-//   $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
-//     data.push({
-//       title: $(elem).text(),
-//       link: $(elem).find('a.imagelink').attr('href'),
-//     });
-//   });
-//   console.log(data);
-// };
-// getData(response.data);
+// accessing the url
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const data = await response.text();
 
-// setting up puppeteer
-// async function run() {
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//   });
-//   await pageUrl.goto(pageUrl);
+let i = '';
+const url = [];
+const str = data;
+const rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
 
-//   await downloadMemeInPage(page);
+while ((i = rex.exec(str))) {
+  url.push(i[1]);
+}
 
-//   browser.close();
-// }
-
-// function fetch(imgUrl) {
-//   console.log(imgUrl);
-// }
-
-// fetch(
-//   'https://memecomplete.com/edit/https:/api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg',
-// ).then((res) => res.body.pipe(fs.createWriteStream('./path/to/memes.jpg')));
-
-// https
-//   .get('https://memegen-link-examples-upleveled.netlify.app/', (response) => {
-//     let data = '';
-//     response.on('data', (chunk) => {
-//       data += chunk;
-//     });
-
-//     response.on('end', () => {
-//       console.log(data);
-//     });
-//   })
-//   .on('error', (error) => {
-//     console.log(error);
-//   });
+// loop for download
+for (let i = 0; i < 10; i++) {
+  if (i < 9) {
+    fs.mkdir('./memes', { recursive: true }, function () {});
+    const fileName = `memes/0${i + 1}.jpg`;
+    console.log(fileName);
+    const file = fs.createWriteStream(fileName);
+    https.get(url[i], function (answer) {
+      answer.pipe(file);
+    });
+  } else {
+    fs.mkdir('./memes', { recursive: true }, function () {});
+    const fileName = `memes/${i + 1}.jpg`;
+    console.log(fileName);
+    const file = fs.createWriteStream(fileName);
+    https.get(url[i], function (answer) {
+      answer.pipe(file);
+    });
+  }
+}
